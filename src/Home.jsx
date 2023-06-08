6af9ce6b5f4b089ec529df35b77be1241920229e
kumuthac23 <kumuthac23@gmail.com>
2023-06-08 06:53:38 +0530

    Categories added

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function Home() {
   //const matches = useMediaQuery((_theme) => _theme?.breakpoints.down("sm"));
@@ -26,6 +28,46 @@ function Home() {
     "https://img.freepik.com/free-psd/horizontal-banner-template-big-sale-with-woman-shopping-bags_23-2148786755.jpg?w=1060&t=st=1686116924~exp=1686117524~hmac=af6331d29c9320731e3798a78b9445b67fdfd7b529df28643b0fac72de5674ad",
     "https://img.freepik.com/free-psd/banner-template-online-shopping_23-2148537543.jpg?w=1060&t=st=1686118829~exp=1686119429~hmac=257d4fb6081b562dc574041345923c92a6304f2659c98cee337f67867a18f258",
     "https://img.freepik.com/premium-psd/summer-sales-banner-template_23-2149015979.jpg?w=1060",
+  ];
+
+
+
+  const settingsProduct = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
+  const categories = [
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "silk Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "cotton Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "art Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "malgodi Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "silk Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "silk Saree",
+    },
+    {
+      img: "https://cdn.shopify.com/s/files/1/0503/7303/4147/products/KP-2053_3_900x1350_crop_center@2x.jpg?v=1660648270",
+      name: "silk Saree",
+    },
   ];
 
   return (
@@ -56,6 +98,63 @@ function Home() {
           </div>
         ))}
       </Slider>
+
+
+
+
+<h2 style={{textAlign:"center"}}>CATEGORIE</h2>
+      <Box
+      sx={{
+        padding: 2,
+      }}
+    >
+      <Box>
+        <Typography
+          sx={{
+            margin: "0px 10px 0px 0px",
+            fontWeight: 700,
+            color: "black",
+            color: "black",
+          }}
+          className="topic-head"
+        >
+          Categories
+        </Typography>
+      </Box>
+
+      <Slider {...settingsProduct}>
+        {categories.map((category, index) => (
+          <Box
+            sx={{
+              paddingRight: "16px",
+              height: "90px",
+              width: "98px",
+            }}
+          >
+            <Card key={index}>
+              <CardMedia
+                sx={{ height: 55, width: 98 }}
+                image={category.img}
+                title="green iguana"
+                component={"img"}
+              />
+              <Box
+                sx={{
+                  padding: "6px",
+                }}
+              >
+                <Typography
+                  sx={{ paddingBottom: 0, fontSize: "medium" }}
+                  variant="h6"
+                >
+                  {category.name}
+                </Typography>
+              </Box>
+            </Card>
+          </Box>
+        ))}
+      </Slider>
+    </Box>
     </div>
   );
 }
