1e0b61059968627688cea35bcc725c2cab7d1893
kumuthac23 <kumuthac23@gmail.com>
2023-06-09 12:08:40 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  // base: "/",
-=======
   base: "/Ecommerce-webapp/",
->>>>>>> 5faee854cde9c39d24b7c3bc046315610787084e
 });
