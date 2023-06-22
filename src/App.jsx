f741ca41e66f1dcc2aed3c6e28d750a84992c7f8
BoobeshPalanisamy <boobeshmalar@gmail.com>
2023-06-22 17:19:39 +0530

    showordersbydate

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,12 @@ import CheckOut from "./CheckOut";
 import Signup from "./Signup";
 import axios from "axios";
 import BagProvider from "./BagContext";
-<<<<<<< HEAD
+import CommonProvider from "./CommonContext";
+import CustomSnackBar from "./CustomSnackBar";
 import ShowOrdersByDate from "./ShowOrdersByDate";
 
 //Only when we deploy the code use this line
-// axios.defaults.baseURL = "https://tam-ecommerce.onrender.com/";
-=======
-import CommonProvider from "./CommonContext";
-import CustomSnackBar from "./CustomSnackBar";
-
-//Only when we deploy the code use this line
 //axios.defaults.baseURL = "https://tam-ecommerce.onrender.com/";
->>>>>>> 996f4fef1958be4d2ab8e84092fef9ebdf688b27
 
 //For Development use this
 axios.defaults.baseURL = "http://localhost:3000/";
@@ -36,36 +30,6 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <BagProvider>
-<<<<<<< HEAD
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />}></Route>
-              <Route path="login" element={<Login />}></Route>
-              <Route path="profile" element={<Profile />}></Route>
-              <Route path="signup" element={<Signup />}></Route>
-              <Route path="orders" element={<Orders />}></Route>
-              <Route
-                path="productDetail/:id"
-                element={<ProductDetail />}
-              ></Route>
-              <Route path="about" element={<About />}></Route>
-              <Route
-                path="shippingaddress"
-                element={<ShippingAddress />}
-              ></Route>
-              <Route path="checkout" element={<CheckOut />}></Route>
-              <Route path="signup" element={<Signup />}></Route>
-              <Route
-                path="productsByCategory/:id"
-                element={<ProductsByCategory />}
-              ></Route>
-              <Route
-                path="showordersbydate"
-                element={<ShowOrdersByDate />}
-              ></Route>
-            </Route>
-          </Routes>
-=======
           <CommonProvider>
             <Routes>
               <Route path="/" element={<Layout />}>
@@ -89,11 +53,14 @@ function App() {
                   path="productsByCategory/:id"
                   element={<ProductsByCategory />}
                 ></Route>
+                <Route
+                  path="showordersbydate"
+                  element={<ShowOrdersByDate />}
+                ></Route>
               </Route>
             </Routes>
             <CustomSnackBar />
           </CommonProvider>
->>>>>>> 996f4fef1958be4d2ab8e84092fef9ebdf688b27
         </BagProvider>
       </ThemeProvider>
     </>
